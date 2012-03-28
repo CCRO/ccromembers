fcb009140a04699a3647e00c540e9adb78262d76
Christopher Jones <crjones@gmail.com>
2012-03-28 14:51:00 -0500

    comment nested under documents

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Ccromembers::Application.routes.draw do
   resources :comments
 
-  resources :documents
-
+  resources :documents do
+    resources :comments
+  end
+  
   resources :companies
 
   match 'login' => 'sessions#new', :as => :login
@@ -60,7 +62,7 @@ Ccromembers::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'reports#dashboard'
+  root :to => 'documents#index'
 
   # See how all your routes lay out with "rake routes"
 
