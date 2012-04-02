6990c830a3777e939e98a57b3a354f924ba5855e
Christopher Jones <crjones@gmail.com>
2012-04-02 14:43:56 -0500

    fixed reference to User in ability class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    user ||= User.new # guest user (not logged in)
+    user ||= Person.new # guest user (not logged in)
     
     if user.member? || user.admin?
       can :read, Document
