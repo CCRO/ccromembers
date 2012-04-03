414cde996c4d28b823bcb2a92494fdc3470633da
Christopher Jones <crjones@gmail.com>
2012-04-03 17:32:04 -0500

    Message, Company, and Person UI cleanup.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,10 @@ class Message < ActiveRecord::Base
   has_many :comments, :as => :commentable
   belongs_to :owner, :polymorphic => true
   belongs_to :author, :class_name => 'Person'
+  
+  scope :sorted_by_activity, :joins => :comments, :group => 'messages.id', :order => "comments.created_at,messages.created_at DESC"
+   
+  def last_activity_time
+    [comments.order(:created_at).last.try(:created_at),self.created_at].compact.first
+  end
 end
