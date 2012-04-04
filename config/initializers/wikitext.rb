fe82d36330bc9904f1becac911fbf771533f673d
Christopher Jones <crjones@gmail.com>
2012-04-04 12:47:37 -0500

    Wikitext rendering now wraps text in <p> and added NYT emphasis.js for highlighting

--- config/initializers/wikitext.rb
+++ config/initializers/wikitext.rb
@@ -2,4 +2,7 @@ class CCROHTML < Wiky::Render::HTML
   def self.process_header(level, text)
     "<h#{level} id=\"#{text.parameterize}\">#{text}</h#{level}>"
   end
+  def self.process_normal(wikitext)
+    "<p>" + super(wikitext) + "</p>"
+  end
 end
