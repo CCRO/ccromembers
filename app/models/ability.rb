b66f7f36c712d5466d960a68390e8cb3aa26a779
Christopher Jones <crjones@gmail.com>
2012-04-13 15:53:33 -0500

    minor change for heroku repush

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
     user ||= Person.new # guest user (not logged in)
     
     if user.member?
