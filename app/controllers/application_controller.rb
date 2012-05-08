06e39f0d8be6f509bad61ae5eda01f733c0a14f0
Joey Lapetina <JoeyLapetina@gmail.com>
2012-05-08 11:00:20 -0500

    Fixed require_admin to check user exist before admin? check. #17

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,7 +60,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_admin
-    redirect_to dashboard_path,:flash => {error: "Not authorized." } unless current_user.admin?
+    redirect_to dashboard_path,:flash => {error: "Not authorized." } unless current_user && current_user.admin?
   end
   
   rescue_from CanCan::AccessDenied do |exception|
