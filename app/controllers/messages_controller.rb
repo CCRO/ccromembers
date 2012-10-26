9126efa932c63aae48128feb7fbc95f97f1024e4
Joey Lapetina <JoeyLapetina@gmail.com>
2012-10-26 12:49:29 -0500

    Some fixes for messages on groups, change to what non-admins see on the group admin bar, Made the group side bar indicate what you were looking at.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -94,7 +94,7 @@ class MessagesController < ApplicationController
     
     respond_to do |format|
       if @message.save
-        format.html { redirect_to @message, notice: 'Message was successfully created.' }
+        format.html { redirect_to polymorphic_path([@group, @message]) }
         format.json { render json: @message, status: :created, location: @message }
       else
         format.html { render action: "new" }
