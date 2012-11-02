96fcb670a721cd9ad507a751785b0e456c9eb080
Christopher Jones <crjones@gmail.com>
2012-11-02 15:42:54 -0500

    Added ability for admin to login as a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -73,6 +73,7 @@ class ApplicationController < ActionController::Base
   helper_method :is_editing?
      
   def current_user
+    current_user ||= Person.find_by_id(session[:su_user_id]) if session[:su_user_id]
     current_user ||= Person.find_by_id(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
