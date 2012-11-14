e58839b20017077ed4ed1d11d1c00eceea070120
Christopher Jones <crjones@gmail.com>
2012-11-14 13:44:15 -0600

    Files not accessible by basic Group members

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,7 +13,7 @@ class Ability
         group.people.include? user
     end
 
-    can :read, [Document, Message, Page, Post] do |object|
+    can :read, [Document, Message, Page, Post, File] do |object|
       object.owner_type == "Group" && object.owner.people.include?(user)
     end
 
