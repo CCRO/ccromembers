08ee5c5025ddda0040ffdf9596e5e04585695c95
Joey Lapetina <JoeyLapetina@gmail.com>
2013-04-02 10:25:46 -0500

    working on new avatar uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -49,9 +49,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "picture"
+  end
+
   def remove_color
     manipulate! do |img|
       img.quantize(256, Magick::GRAYColorspace)
