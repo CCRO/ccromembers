35fb8d6a1612c50f0cceb0693c04646d5fb6e253
Joey Lapetina <JoeyLapetina@gmail.com>
2013-04-02 11:14:04 -0500

    working on picture uploads

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -50,7 +50,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    "picture"
+    "picture" if original_filename
   end
 
   def remove_color
