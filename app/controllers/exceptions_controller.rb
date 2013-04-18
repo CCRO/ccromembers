f4bcacdeb21163b440079e69e0983a0cf0a8bdc3
Joey Lapetina <JoeyLapetina@gmail.com>
2013-04-18 15:57:41 -0500

    put in mobile login

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -1,5 +1,13 @@
 class ExceptionsController < ApplicationController
+
+  has_mobile_fu
+
   def accessdenied
   	cookies[:url_after_signup] = session[:url_return_to]
+
+    respond_to do |format|
+      format.mobile {render :layout => '/layouts/blank.html.erb'}
+      format.html
+    end
   end
 end
