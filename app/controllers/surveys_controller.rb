58e091f8d655769b0e09768833e44a292c05a298
Joey Lapetina <joeylapetina@gmail.com>
2013-08-08 13:46:17 -0500

    added in a way to make sure the section exists

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -21,6 +21,9 @@ class SurveysController < ApplicationController
     @survey = Survey.find(params[:id])
     @response = Response.new
     @section_id = params[:section_id] if params[:section_id]
+    if @section_id.to_i > @survey.questions.where(title: true).length
+      @section_id = "0"
+    end
     
     authorize! :read, @survey
 
